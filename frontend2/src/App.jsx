8f7e2cb68ef2707ce853ba28334235d4a69788bc
Ved Prakash Vishwakarma <vedprakash22@iitk.ac.in>
2025-03-08 21:23:38 +0530

    Admin page created + Backend folder created and some setup done + Routes added to the frontend

--- frontend2/src/App.jsx
+++ frontend2/src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
+import Admin from "./pages/Admin";
 import Login from "./pages/Login";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Login></Login>
+      <Routes>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/admin" element={<Admin></Admin>}></Route>
+      </Routes>
     </div>
   );
 }
