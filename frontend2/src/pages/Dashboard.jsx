0a42ba2dd05fda0a1a7c8234d41d8d8d0d75ea4b
Ved Prakash Vishwakarma <vedprakash22@iitk.ac.in>
2025-03-09 00:12:07 +0530

    dashboard created

--- frontend2/src/pages/Dashboard.jsx
+++ frontend2/src/pages/Dashboard.jsx
@@ -23,6 +23,8 @@ export default function Dashboard() {
     {code: 'EE370', name: 'Digital Electronics', prof:"Shubham Sahay"},
   ];
 
+  const pastYears = ["2024-25 SEMII", "2024-25 SEMI", "2024-25 SEMII", "2024-25 SEMI", "2024-25 SEMII", "2024-25 SEMI"];
+
   return (
     <div className='h-screen w-full flex items-center bg-[#F5F5F5]'>
 
@@ -45,11 +47,11 @@ export default function Dashboard() {
       </div>
 
       <div className='w-full h-full flex flex-col items-center justify-evenly'>
-        <p className='text-[35px] font-semibold '>Dashboard</p>
+        <p className='text-[35px] font-semibold mt-2'>Dashboard</p>
         <div className='flex gap-3 mt-4'>
           {
             courses.map(course=>(
-                <div key={course.code} className='cursor-pointer hover:scale-95 transition-all duration-200 h-[130px] w-[180px] border border-black rounded-md flex flex-col items-center justify-evenly p-2'>
+                <div key={course.code} className='cursor-pointer shadow-lg hover:scale-95 transition-all duration-200 h-[130px] w-[180px] border  rounded-md flex flex-col items-center justify-evenly p-2'>
                     <p className='font-semibold bg-[#D9D9D9] px-5 py-1 rounded-md'>{course.code}</p>
                     <p>{course.prof}</p>
                     <p className='text-[14px]'>{course.name.substring(0,14)+"..."}</p>
@@ -58,8 +60,16 @@ export default function Dashboard() {
           }
         </div>
 
-        <iframe src="https://calendar.google.com/calendar/embed?height=550&wkst=1&ctz=Asia%2FKolkata&mode=WEEK&showTz=0&showTitle=0&showPrint=0&src=dmVkdmlzaHdha2FybWEyMjZAZ21haWwuY29t&src=ZW4uaW5kaWFuI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%230B8043" className='w-[92%] m-auto h-[550px] border rounded-lg'></iframe>
-      </div>
+        <iframe src="https://calendar.google.com/calendar/embed?height=550&wkst=1&ctz=Asia%2FKolkata&mode=WEEK&showTz=0&showTitle=0&showPrint=0&src=dmVkdmlzaHdha2FybWEyMjZAZ21haWwuY29t&src=ZW4uaW5kaWFuI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%230B8043" className='w-[92%] m-auto h-[520px] border rounded-lg shadow-xl'></iframe>
+
+        <div className='flex gap-3 w-[92%] justify-evenly items-center mb-6'>
+            {
+                pastYears.map(year=>(
+                    <div className='bg-white border cursor-pointer py-3 px-6 rounded-lg shadow-lg hover:scale-95 transition-all duration-200'>{year}</div>
+                ))
+            }
+        </div>
+      </div> 
 
     </div>
   )
--- frontend2/tailwind.config.js
+++ frontend2/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        '3xl': '-11px 10px 14px 3px rgba(0, 0, 0, 0.3)',
+      },
+    },
   },
   plugins: [],
 };
