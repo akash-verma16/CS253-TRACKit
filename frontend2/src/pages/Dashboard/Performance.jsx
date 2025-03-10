a332598fd8fca8789d8c49d301c8f1905480292c
vdhruv221 <vdhruv22@iitk.ac.in>
2025-03-10 13:52:32 +0530

    forum added

--- frontend2/src/pages/Dashboard/Performance.jsx
+++ frontend2/src/pages/Dashboard/Performance.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function Performance() {
   return (
-    <div>
-      Performance
+    <div className='w-full h-full flex flex-col items-start justify-start px-6'>
+      <p className='text-[35px] font-bold mt-2 self-start'>Performance</p>
+      <p className='font-semibold bg-[#D9D9D9] px-5 py-1 rounded-md' style={{ fontSize: '20px' }}>Template</p>
+      <iframe src=''></iframe>
     </div>
   )
 }
