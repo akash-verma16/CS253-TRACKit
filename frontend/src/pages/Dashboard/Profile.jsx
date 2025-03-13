2f326144c688437ad4f81dfe903d10f45bed4294
aayushs22 <aayushs22@iitk.ac.in>
2025-03-13 14:18:15 +0530

    added functionality so that names etc are always updated on new sign-in. used react hooks.

--- frontend/src/pages/Dashboard/Profile.jsx
+++ frontend/src/pages/Dashboard/Profile.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import imgMy from '../../assets/ved.png'
-import texture from '../../assets/textures.jpg'
-
-const user = JSON.parse(localStorage.getItem('user'));
+import React, { useState, useEffect } from 'react';
+import imgMy from '../../assets/ved.png';
+import texture from '../../assets/textures.jpg';
 
 export default function Profile() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const storedUser = JSON.parse(localStorage.getItem('user'));
+    setUser(storedUser);
+  }, []);
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className='w-full h-full flex items-center justify-center'>
         <div className='relative w-[80%] h-[80%] border shadow-2xl rounded-xl'>
@@ -20,12 +29,12 @@ export default function Profile() {
             <img src={imgMy} alt="img" className='w-[130px] aspect-square rounded-full absolute right-[42%] top-[29%]' />
 
             <div>
-                <p className='text-[50px] mt-[60px] text-center'> {user.firstName} {user.lastName}</p>
+                <p className='text-[50px] mt-[60px] text-center'>{user.firstName} {user.lastName}</p>
                 <p className='text-center text-[18px] mt-6'>
-                    <span className='font-bold'>Roll No :</span> <span>220024</span>
+                    <span className='font-bold'>Roll No :</span> <span>{user.rollNo}</span>
                 </p>
                 <p className='text-center text-[18px] mt-2'>
-                    <span className='font-bold'>Department :</span> <span>EE</span>
+                    <span className='font-bold'>Department :</span> <span>{user.department}</span>
                 </p>
             </div>
         </div>
