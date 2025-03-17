8eec2bf46ad73784aee000adb8e082e301a6973f
Ved Prakash Vishwakarma <vedprakash22@iitk.ac.in>
2025-03-17 14:27:21 +0530

    Initialization of Coursecontext

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
-import { fetchUserProfile, API_URL } from '../services/api';
+import { fetchUserProfile, API_URL } from '../services/auth';
 
 // Create auth context
 const AuthContext = createContext();
